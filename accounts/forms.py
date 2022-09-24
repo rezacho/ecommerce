aae82394d2b9118140209ea47f912a59b0e7204e
rezacho <reza.chookian@gmail.com>
2022-09-25 00:39:33 +0330

    create class UserChangeForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 
 class UserCreationForm(forms.ModelForm):
@@ -24,3 +25,11 @@ class UserCreationForm(forms.ModelForm):
 		if commit:
 			user.save()
 		return user
+
+
+class UserChangeForm(forms.ModelForm):
+	password = ReadOnlyPasswordHashField(help_text="Change password using <a href=\"../password/\">this form</a>.")
+
+	class Meta:
+		model = User
+		fields = ('email', 'phone_number', 'full_name', 'password', 'last_login')
