92362b3148af6b4fa82cde4fc38c24886635b278
rezacho <reza.chookian@gmail.com>
2022-09-25 16:29:16 +0330

    create class UserRegisterView def get

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import UserRegistrationForm
 
-# Create your views here.
+
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, 'register.html', {'form': form})
+
