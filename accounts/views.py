4263b8e9fd2da5e9cc89cd0fde5445aecf578405
rezacho <reza.chookian@gmail.com>
2022-09-26 12:54:33 +0330

    create class UserRegistrationView def post

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
+from models import OtpCode
 from .forms import UserRegistrationForm
+from utils import send_otp_code
+import random
 
 
 class UserRegisterView(View):
@@ -9,3 +13,19 @@ class UserRegisterView(View):
     def get(self, request):
         form = self.form_class
         return render(request, 'accounts/register.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class
+        if form.is_valid():
+            random_code = random.randint(1000, 9999)
+            send_otp_code(form.cleaned_data['phone_number'], code=random_code)
+            OtpCode.objects.create(phone_number=form.cleaned_data['phone_number'], code=random_code)
+            request.session['user_registration_info'] = {
+                'phone_number': form.cleaned_data['phone_number'],
+                'email': form.cleaned_data['email'],
+                'full_name': form.cleaned_data['full_name'],
+                'password': form.cleaned_data['password'],
+            }
+            messages.success(request, 'We sent you verification code ', 'success')
+            return redirect('accounts:verify_code')
+        return redirect('home:home')
