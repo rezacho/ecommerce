731533821421d5574e6033130c76671e7dfb5215
rezacho <reza.chookian@gmail.com>
2022-09-27 17:04:46 +0330

    edit class UserRegistrationForm add (def clean_email - def clean_phone)

--- accounts/forms.py
+++ accounts/forms.py
@@ -41,7 +41,20 @@ class UserRegistrationForm(forms.Form):
 	phone_number = forms.CharField(max_length=11)
 	password = forms.CharField(widget=forms.PasswordInput)
 
+	def clean_email(self):
+		email = self.cleaned_data['email']
+		user = User.objects.filter(email=email).exists()
+		if user:
+			raise ValidationError('This email already exists')
+		return email
+
+	def clean_phone(self):
+		phone = self.cleaned_data['phone_number']
+		user = User.objects.filter(phone_number=phone).exists()
+		if user:
+			raise ValidationError('This phone number already exists')
+		return phone
+
 
 class VerifyCodeForm(forms.Form):
 	code = forms.IntegerField()
-
