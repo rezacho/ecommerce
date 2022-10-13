cfc61b7dfa7acb806404b73a25b111efd8e8dc3e
rezacho <reza.chookian@gmail.com>
2022-10-14 02:03:03 +0330

    create class ProductDetailView def get

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Product
 
@@ -7,3 +7,9 @@ class HomeView(View):
     def get(self, request):
         products = Product.objects.filter(is_available=True)
         return render(request, 'home.html', {'products': products})
+
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'detail.html', {'product': product})
